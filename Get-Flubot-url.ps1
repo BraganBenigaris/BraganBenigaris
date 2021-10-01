0c6a41d2be5c55d244a8ebb4c53fa4ced7fb292e
BraganBenigaris <91657243+BraganBenigaris@users.noreply.github.com>
2021-10-01 14:44:07 +0200

    Delete Get-Flubot-url.ps1

--- Get-Flubot-url.ps1
+++
@@ -1,8 +0,0 @@
-$dnslist = Get-Content ./dnslist.txt
-
-$Results = foreach ($villain in $dnslist)
-{
-    Resolve-DNSName -type A -Name $villain -DnsOnly -QuickTimeout -ErrorAction SilentlyContinue 
-}
-
-$Results | Select Name,IPAddress | Export-Csv c:\temp\dga$((Get-date).ToString("yyyyMMdd")).csv -NoTypeInformation -Delimiter ";"
